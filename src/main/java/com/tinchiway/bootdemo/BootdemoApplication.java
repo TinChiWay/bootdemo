a0726b70bdd99e8df45dd270c5eaab044bc7e008
田志伟 <tianzhiwei@fangdd.com>
2019-01-01 13:20:44 +0800

    feat(init): init the bootdemp

---
+++ src/main/java/com/tinchiway/bootdemo/BootdemoApplication.java
@@ -0,0 +1,16 @@
+package com.tinchiway.bootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+
+public class BootdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootdemoApplication.class, args);
+	}
+
+}
+
---
+++ src/main/java/com/tinchiway/bootdemo/controller/DemoController.java
@@ -0,0 +1,12 @@
+package com.tinchiway.bootdemo.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author tinchiway
+ * @date 2018-12-29
+ */
+
+@RestController
+public class DemoController {
+}
